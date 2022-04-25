4fe03a29b5793e17252b756b07d4a0c2ee3eee5d
moonjdoob <gitvics@gmail.com>
2022-04-25 11:12:17 +0530

    datatype work

---
+++ datatype.py
@@ -0,0 +1,24 @@
+# In programming, data type is an important concept.
+# Variables can store data of different types, and different types can do different things.
+# Python has the following data types built-in by default, in these categories.
+
+# Text Type:str
+# Numeric Types:int, float, complex
+# Sequence Types:list, tuple, range
+# Mapping Type:dict
+# Set Types:set, frozenset
+# Boolean Type:bool
+# Binary Types:bytes, bytearray, memoryview
+
+# type() function used to get datatype. python datatype will be set when assign value to a variable.
+
+
+x = 5
+print(type(x))
+
+
+#Setting the Specific Data Type
+
+s = "Hello, Atul"
+print(type(s))
+
